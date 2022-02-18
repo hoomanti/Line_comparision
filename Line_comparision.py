6680b7ccc574bbcdeb24d67a0b3713459377242f
hoomanti <manisha.edu@protonmail.com>
2022-02-19 01:00:34 +0530

    Compare two lines on the babis of end points

--- Line_comparision.py
+++ Line_comparision.py
@@ -11,10 +11,9 @@ length_of_line1 = math.sqrt(((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)))
 print(length_of_line1)
 length_of_line2 = math.sqrt(((q2 - q1) * (q2 - q1) + (p2 - p1) * (p2 - p1)))
 print(length_of_line2)
-if length_of_line1 == length_of_line2:
+if length_of_line1 is length_of_line2:
     print("Length of both the lines are equal.")
 elif length_of_line1 < length_of_line2:
     print("Second line is greater than first line.")
 else:
     print("First line is greater than second line.")
-
