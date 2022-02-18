5e5cb15bd2d15cc217259bb0090e84db8f1a75fd
hoomanti <manisha.edu@protonmail.com>
2022-02-19 00:56:36 +0530

    Compare two lines to know line is equal, greater or less then the other line

--- Line_comparision.py
+++ Line_comparision.py
@@ -4,13 +4,17 @@ import math
 x1, x2, y1, y2 = 3, 8, 4, 9
 
 # assigning value to second line
-p1, p2, q1, q2 = 12, 17, 13, 18
+p1, p2, q1, q2 = 8, 13, 10, 12
 
 # calculate length of both the lines
 length_of_line1 = math.sqrt(((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)))
 print(length_of_line1)
 length_of_line2 = math.sqrt(((q2 - q1) * (q2 - q1) + (p2 - p1) * (p2 - p1)))
 print(length_of_line2)
-
 if length_of_line1 == length_of_line2:
-    print("Length of both the lines are equal")
+    print("Length of both the lines are equal.")
+elif length_of_line1 < length_of_line2:
+    print("Second line is greater than first line.")
+else:
+    print("First line is greater than second line.")
+
