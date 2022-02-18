708ecb7dbbcf82884c0af05f1a9e1aaed7827a1e
hoomanti <manisha.edu@protonmail.com>
2022-02-19 00:47:40 +0530

    Model a line based on points using Cartesian System

--- Line_comparision.py
+++ Line_comparision.py
@@ -1,1 +1,7 @@
-print("Welcome to Line Comparision Computation program on Master Branch")
+import math
+
+x_coordinate1, x_coordinate2, y_coordinate1, y_coordinate2 = 1, 5, 2, 6
+length_of_line = math.sqrt(((y_coordinate2 - y_coordinate1) * (y_coordinate2 - y_coordinate1) +
+                            (x_coordinate2 - x_coordinate1) * (x_coordinate2 * x_coordinate1)))
+print(length_of_line)
+
